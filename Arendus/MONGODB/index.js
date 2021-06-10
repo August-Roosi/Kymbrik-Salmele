7dc597472a4e307cfdca50f18cd7146fb28c1d49
AugustR3 <augustroosi@gmail.com>
2021-06-10 22:43:25 +0300

    Peamiselt failide strukturiseerimine ning andmebaasi ühendamine HTML-i ja CSS-iga.

--- Arendus/MONGODB/index.js
+++ Arendus/MONGODB/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const app = express();
-const path = require('path');
-const Product = require('./models/product')
+const Product = require('./models/product');
 
 mongoose.connect('mongodb://localhost:27017/productStore', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
@@ -13,13 +13,33 @@ mongoose.connect('mongodb://localhost:27017/productStore', {useNewUrlParser: tru
         console.log(err)
     })
 
+app.use(express.static('public'))
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req, res) => {
-    res.send("WOOF!")
+app.get('/K%C3%BCmbrik' , (req, res) => {
+    res.render('MAIN.ejs')
+})
+
+
+app.get('/K%C3%BCmbrik/pood', (req, res) => {
+    res.render('POOD.ejs')
+})
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find({}) 
+    console.log(products)
+    res.send("Delivering thy products...")
+})
+
+app.get('*', (req, res) => {
+    res.send("INVALID URL :8")
 })
 
 app.listen(3000, () => {
     console.log("App is listening on port 3000")
 })
+
+
